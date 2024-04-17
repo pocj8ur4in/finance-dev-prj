212a0fdc3d52083f8ed7dcd044abea15a6adec50
pocj8ur4in <pocj8ur4in@gmail.com>
2024-04-17 20:06:42 +0900

    init/common: 커스텀 어노테이션 생성

---
+++ common/src/main/java/finance/dev/common/annotation/TypeInfo.java
@@ -0,0 +1,18 @@
+package finance.dev.common.annotation;
+
+import java.lang.annotation.*;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+@Target({ElementType.TYPE}) // 어노테이션을 사용할 수 있는 대상을 클래스, 인터페이스, 열거형으로 지정
+@Retention(RetentionPolicy.RUNTIME) // 어노테이션 정보를 런타임 시간까지 유지하도록 설정
+@Documented // 해당 어노테이션을 javadoc에 포함시킴
+@Component
+public @interface TypeInfo {
+    @AliasFor(annotation = Component.class)
+    String value() default ""; // 해당 어노테이션을 스프링 컨테이너의 빈으로 등록할 때 사용할 이름
+
+    String name(); // 클래스, 인터페이스, 열거형 이름
+
+    String description(); // 클래스, 인터페이스, 열거형 설명
+}
